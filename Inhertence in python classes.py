d6ae9afbb63af02a1411bf3992af85819dca3d39
RomishShrestha <romishshrestha@gmail.com>
2023-01-28 00:05:24 +1100

    defining dtype within class

--- Inhertence in python classes.py
+++ Inhertence in python classes.py
@@ -2,24 +2,38 @@
 #A CHILD CLASS CAN INHERIT THE METHODS AND ATTRIBUTES FROM A PARENT CLASS
 
 class Person():
+
+    name: str  #define dtype
+    age: int
+    fav_colour: str
+    year: int
+
     def __init__(self, name, age, fav_colour, year):
         self.name =name
         self.age =age
         self.fav_colour = fav_colour
         self.year = year
 
+
+
 #method to calc year of birth
-    def year_of_birth(self):
+    def year_of_birth(self) ->int:
         return 2023-self.age
     
 #method to calc projected age
 
-    def project_age(self, projection_year):
+    def project_age(self, projection_year) -> int:
         return (projection_year -self.year) + self.age
+    
 
+    
 
+#test
 
 new_person = Person(name= 'Renan Shrestha', age= 6, fav_colour = 'white', year= 2023)
 print(new_person.year_of_birth())
 
 print(new_person.project_age(projection_year= 2045))
+
+person1 = Person(name ='Romish Shrestha', age= 39, fav_colour= 'black-blue', year =2022)
+print(person1.year_of_birth())
