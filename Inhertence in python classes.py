b360d272b5cb74b0f58d1ddf6342c003dcd739b4
RomishShrestha <romishshrestha@gmail.com>
2023-01-28 00:10:23 +1100

    dtype and setting default values in class

--- Inhertence in python classes.py
+++ Inhertence in python classes.py
@@ -7,12 +7,14 @@ class Person():
     age: int
     fav_colour: str
     year: int
+    human: bool =True #setting dtype and default value
 
-    def __init__(self, name, age, fav_colour, year):
+    def __init__(self, name, age, fav_colour, year, human):
         self.name =name
         self.age =age
         self.fav_colour = fav_colour
         self.year = year
+        self.human = human
 
 
 
@@ -30,10 +32,12 @@ class Person():
 
 #test
 
-new_person = Person(name= 'Renan Shrestha', age= 6, fav_colour = 'white', year= 2023)
+new_person = Person(name= 'Renan Shrestha', age= 6, fav_colour = 'white', year= 2023, human =True)
 print(new_person.year_of_birth())
 
 print(new_person.project_age(projection_year= 2045))
 
-person1 = Person(name ='Romish Shrestha', age= 39, fav_colour= 'black-blue', year =2022)
+person1 = Person(name ='Romish Shrestha', age= 39, fav_colour= 'black-blue', year =2022, human =False)
 print(person1.year_of_birth())
+
+print(person1.human)
